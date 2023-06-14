b7d765f217d8814f77f2cc684837b2a1ed042179
ocar1053 <ocar8951@gmail.com>
2023-06-15 01:15:43 +0800

    Update delete_polls.py

--- delete_polls.py
+++ delete_polls.py
@@ -6,14 +6,14 @@ from flask import jsonify, Blueprint
 bp = Blueprint('delete_polls', __name__)
 
 
-@bp.route('/delete_poll/<path:poll_id>', methods=['DELETE'])
+@bp.route('/admin/delete_poll/<path:poll_id>', methods=['DELETE'])
 @login_required
 def delete_poll(poll_id):
     poll = Poll.query.get(poll_id)
     if not poll:
         return jsonify({"error": "Poll not found."}), 404
 
-    if poll.creator_id != current_user.user_id or poll.is_approved:
+    if current_user.email != 'admin@nccu.edu.tw' or poll.is_approved:
         return jsonify({"error": "Unauthorized operation."}), 403
 
     db.session.delete(poll)
